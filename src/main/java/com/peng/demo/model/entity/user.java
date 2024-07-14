6a926095c775e9a43d1988b4fa065c4962e59e24
bigpeng <1015792002@qq.com>
2024-07-14 12:06:12 +0800

    2024-07-14 12:00 commit by peng

---
+++ src/main/java/com/peng/demo/model/entity/user.java
@@ -0,0 +1,65 @@
+package com.peng.demo.model.entity;
+
+import java.sql.Timestamp;
+
+public class user {
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private Integer status;
+
+    private Timestamp create_time;
+
+    private Timestamp update_time;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Timestamp getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(Timestamp create_time) {
+        this.create_time = create_time;
+    }
+
+    public Timestamp getUpdate_time() {
+        return update_time;
+    }
+
+    public void setUpdate_time(Timestamp update_time) {
+        this.update_time = update_time;
+    }
+}
