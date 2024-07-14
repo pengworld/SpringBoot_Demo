75008a3b44a4cb24d315aed36cfe8c4175e57868
bigpeng <1015792002@qq.com>
2024-07-14 10:57:24 +0800

    2024-07-14 10:50 commit by peng

--- src/main/java/com/peng/demo/DemoApplication.java
+++ src/main/java/com/peng/demo/DemoApplication.java
@@ -1,4 +1,12 @@
 package com.peng.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class DemoApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(DemoApplication.class,args);
+    }
 }
