2d4194ee56128312f0816cdda4c5b47ef1a0678e
bigpeng <1015792002@qq.com>
2024-07-14 12:56:02 +0800

    2024-07-14 12:30 commit by peng

--- src/main/java/com/peng/demo/controller/UserLoginController.java
+++ src/main/java/com/peng/demo/controller/UserLoginController.java
@@ -1,4 +1,34 @@
 package com.peng.demo.controller;
 
+import com.peng.demo.common.Result;
+import com.peng.demo.model.entity.User;
+import com.peng.demo.service.UserLoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
 public class UserLoginController {
+
+    @Autowired
+    private UserLoginService userLoginService;
+
+    @PostMapping(value="/register")
+    public Result register(@RequestBody User user) {
+        Integer count = userLoginService.registerUser(user);
+
+        Result result = new Result();
+        if (count > 0){
+            result.setMsg("用户注册成功");
+            result.setSuccess(true);
+            result.setDetail(user);
+        }else{
+            result.setMsg("用户注册失败");
+            result.setSuccess(false);
+        }
+        return result;
+    }
 }
