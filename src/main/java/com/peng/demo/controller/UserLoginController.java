e91fe460ac34deb9437fa553e5ecfc3ec3e8508e
bigpeng <1015792002@qq.com>
2024-07-14 14:10:31 +0800

    2024-07-14 12:30 commit by peng

--- src/main/java/com/peng/demo/controller/UserLoginController.java
+++ src/main/java/com/peng/demo/controller/UserLoginController.java
@@ -4,12 +4,12 @@ import com.peng.demo.common.Result;
 import com.peng.demo.model.entity.User;
 import com.peng.demo.service.UserLoginService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/login")
 public class UserLoginController {
 
